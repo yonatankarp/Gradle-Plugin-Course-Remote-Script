1f8a4aab1ba341f647e89dfbd28fd8293e3a5cde
Yonatan Karp-Rudin <yonatan.karp@sumup.com>
2023-07-18 13:05:14 +0200

    Add possibility to filter by file first letter
    This commit adds the possibility to sort the files by the 1st letter of the file name

--- filesPlugin.gradle.kts
+++ filesPlugin.gradle.kts
@@ -13,80 +13,85 @@ logger.quiet("==================================================================
 
 
 tasks.register("sortFiles") {
-  group = "files"
-  description = "Sorts files in a directory into build/files based on the sorting type (date/extension)"
-  dependsOn("cleanFiles")
+    group = "files"
+    description = "Sorts files in a directory into build/files based on the sorting type (date/extension)"
+    dependsOn("cleanFiles")
 
-  doLast {
-    logger.quiet("==== Sorting files ====")
+    doLast {
+        logger.quiet("==== Sorting files ====")
+        logger.newLine()
+
+        val sortType = project.properties["tasks.files.sortType"].toString()
+
+        file(project.properties["tasks.files.folder"].toString())
+            .listFiles { file -> file.isFile && !file.isHidden }
+            ?.forEach { file ->
+                logger.quiet("📂 Filename: ${file.name}")
+
+                // Get directory for the file based on the sorting type
+                val fileMapper = getFileDirectoryMapper(sortType, logger)
+                val directory = fileMapper.getDirectory(file)
+
+                // Create new directory inside build folder
+                val output = project
+                    .layout
+                    .buildDirectory
+                    .dir("files/$directory")
+                    .get()
+                    .asFile
+                    .also { it.mkdirs() }
+
+                // Copy files to sorted directory inside build directory
+                Files.copy(file.toPath(), File(output, file.name).toPath(), StandardCopyOption.REPLACE_EXISTING)
+            }
+    }
+
     logger.newLine()
-
-    val sortType = project.properties["tasks.files.sortType"].toString()
-
-    file(project.properties["tasks.files.folder"].toString())
-      .listFiles { file -> file.isFile && !file.isHidden }
-      ?.forEach { file ->
-        logger.quiet("📂 Filename: ${file.name}")
-
-        // Get directory for the file based on the sorting type
-        val fileMapper = getFileDirectoryMapper(sortType, logger)
-        val directory = fileMapper.getDirectory(file)
-
-        // Create new directory inside build folder
-        val output = project
-          .layout
-          .buildDirectory
-          .dir("files/$directory")
-          .get()
-          .asFile
-          .also { it.mkdirs() }
-
-        // Copy files to sorted directory inside build directory
-        Files.copy(file.toPath(), File(output, file.name).toPath(), StandardCopyOption.REPLACE_EXISTING)
-      }
-  }
-
-  logger.newLine()
-  logger.quiet("==== Files sorted successfully 🎉 ====")
+    logger.quiet("==== Files sorted successfully 🎉 ====")
 }
 
 tasks.register<Delete>("cleanFiles") {
-  group = "files"
-  description = "Clean build directory"
+    group = "files"
+    description = "Clean build directory"
 
-  delete {
-    delete(project.layout.buildDirectory.dir("files").get())
-  }
+    delete {
+        delete(project.layout.buildDirectory.dir("files").get())
+    }
 }
 
 fun getFileDirectoryMapper(sortType: String, logger: Logger): FileDirectoryMapper {
-  when (sortType) {
-    "date" -> return FileDirectoryDateMapper()
-    "extension" -> return FileDirectoryExtensionMapper()
-    else -> {
-      logger.quiet("⚠️ Property [tasks.files.sortType] isn't set or contains invalid value, default sorting will be done by creation date")
-      return FileDirectoryDateMapper()
+    return when (sortType) {
+        "date" -> FileDirectoryDateMapper()
+        "extension" -> FileDirectoryExtensionMapper()
+        "alphabet" -> FileDirectoryAlphabeticMapper()
+        else -> {
+            logger.quiet("⚠️ Property [tasks.files.sortType] isn't set or contains invalid value, default sorting will be done by creation date")
+            FileDirectoryDateMapper()
+        }
     }
-  }
 }
 
 
 interface FileDirectoryMapper {
-  fun getDirectory(file: File): String
+    fun getDirectory(file: File): String
 }
 
 class FileDirectoryDateMapper : FileDirectoryMapper {
-  override fun getDirectory(file: File): String {
-    val creationTime = Files.getAttribute(Paths.get(file.path), "creationTime") as FileTime
-    val fileCreationDateFormat = DateTimeFormatter.ofPattern("MM-YYYY")
-    return fileCreationDateFormat.format(
-      Instant.ofEpochMilli(creationTime.toMillis()).atOffset(ZoneOffset.UTC).toLocalDate()
-    )
-  }
+    override fun getDirectory(file: File): String {
+        val creationTime = Files.getAttribute(Paths.get(file.path), "creationTime") as FileTime
+        val fileCreationDateFormat = DateTimeFormatter.ofPattern("MM-YYYY")
+        return fileCreationDateFormat.format(
+            Instant.ofEpochMilli(creationTime.toMillis()).atOffset(ZoneOffset.UTC).toLocalDate()
+        )
+    }
 }
 
 class FileDirectoryExtensionMapper : FileDirectoryMapper {
-  override fun getDirectory(file: File): String = file.extension
+    override fun getDirectory(file: File): String = file.extension
+}
+
+class FileDirectoryAlphabeticMapper : FileDirectoryMapper {
+    override fun getDirectory(file: File): String = file.name.first().toString()
 }
 
 fun Logger.newLine() = this.quiet("")
